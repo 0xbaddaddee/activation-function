686d989aaa1e5c3e2cd537d6a01a44ecf13c24d8
beelzebufo <zzeds_dead@yahoo.com>
2021-07-26 23:51:57 +0200

    Add LeakyReLU activation function

--- LeakyReLU.py
+++ LeakyReLU.py
@@ -12,4 +12,4 @@ class LeakyReLU:
         return np.where(input_tensor > 0, input_tensor, self.alpha * input_tensor)
 
     def backward(self, error_tensor):
-        return np.where(self.input_tensor > 0, error_tensor, self.alpha * error_tensor)
+        return np.where(self.input_tensor > 0, error_tensor, self.alpha * error_tensor
