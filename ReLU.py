753ee1dbec6bbd4a80c3e4ec5f24e846131e983e
beelzebufo <zzeds_dead@yahoo.com>
2021-07-26 23:51:13 +0200

    Add ReLU activation function

--- ReLU.py
+++ ReLU.py
@@ -11,4 +11,4 @@ class ReLU:
         return np.fmax(self.input_tensor, 0.0)
 
     def backward(self, error_tensor):
-        return np.where(self.input_tensor <= 0, 0, error_tensor
+        return np.where(self.input_tensor <= 0, 0, error_tensor)
